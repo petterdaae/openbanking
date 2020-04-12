77c1b175164fec554f3ebfb9e3938589b8b79ac7
Petter Daae <petter.daae@gmail.com>
2020-04-12 13:21:17 +0200

    Create daterange indeication

--- app/lib/views/spendings.dart
+++ app/lib/views/spendings.dart
@@ -1,15 +1,76 @@
-import 'package:app/components/bar_chart.dart';
+import 'package:app/auth.dart';
+import 'package:app/components/category_list.dart';
+import 'package:app/dependencies.dart';
+import 'package:app/models/category.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Spendings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: BarChartComponent(
-        barWidth: 20,
-        values: [1, 2, 3, 4, 5],
-        categories: ["food", "transport", "alcohol", "food", "transport"],
-      ),
+    Firestore firestore = Provider.of<Dependencies>(context).firestore;
+    String uid = Provider.of<Auth>(context).uid;
+    return StreamBuilder<QuerySnapshot>(
+      stream: firestore
+          .collection('users')
+          .document(uid)
+          .collection('categories')
+          .snapshots(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<QuerySnapshot> snapshot,
+      ) {
+        if (!snapshot.hasData) return Text("loading");
+        final List<Category> categories =
+            snapshot.data.documents.map((c) => Category.parse(c)).toList();
+        return Column(
+          children: <Widget>[
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "12. march",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                        child: Divider(
+                          color: Colors.blue,
+                          thickness: 2,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "12.april",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: CategoryListComponent(
+                categories: categories,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
