05729bc5e9ea427cf9a89a0e12587f7657eff975
Petter Daae <petter.daae@gmail.com>
2020-05-02 21:36:31 +0200

    Paginate transaction list

--- app/lib/views/all_transactions.dart
+++ app/lib/views/all_transactions.dart
@@ -1,54 +1,78 @@
 import 'package:app/auth.dart';
-import 'package:app/components/transaction_list.dart';
+import 'package:app/components/firestore_pagination.dart';
+import 'package:app/components/transaction_list_item.dart';
 import 'package:app/dependencies.dart';
 import 'package:app/models/category.dart';
+import 'package:app/models/transaction.dart' as T;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
-import '../models/transaction.dart' as T;
-import '../utilities.dart';
+class AllTransactions extends StatefulWidget {
+  @override
+  _AllTransactionsState createState() => _AllTransactionsState();
+}
 
-class AllTransactions extends StatelessWidget {
+class _AllTransactionsState extends State<AllTransactions> {
+  int limit;
+
+  @override
+  void initState() {
+    super.initState();
+    this.limit = 20;
+  }
+
   @override
   Widget build(BuildContext context) {
     Firestore firestore = Provider.of<Dependencies>(context).firestore;
     String uid = Provider.of<Auth>(context).uid;
-    return StreamBuilder2<QuerySnapshot, QuerySnapshot>(
-      stream1: firestore
-          .collection('users')
-          .document(uid)
-          .collection('transactions')
-          .orderBy('accountingDate', descending: true)
-          .limit(100)
-          .snapshots(),
-      stream2: firestore
+    return StreamBuilder<QuerySnapshot>(
+      stream: firestore
           .collection('users')
           .document(uid)
           .collection('categories')
           .snapshots(),
       builder: (
         BuildContext context,
-        AsyncSnapshot<QuerySnapshot> transactionsSnapshot,
         AsyncSnapshot<QuerySnapshot> categoriesSnapshot,
       ) {
-        if (!transactionsSnapshot.hasData || !categoriesSnapshot.hasData) {
+        if (!categoriesSnapshot.hasData) {
           return Text("Loading");
         }
-
-        List<T.Transaction> transactions = transactionsSnapshot.data.documents
-            .map((transaction) => T.Transaction.parse(transaction))
-            .toList();
 
         List<Category> categories = categoriesSnapshot.data.documents
             .map((category) => Category.parse(category))
             .toList();
 
-        return TransactionListComponent(
-          transactions: transactions,
-          categories: categories,
+        return FirestorePagination(
+          query: firestore
+              .collection('users')
+              .document(uid)
+              .collection('transactions')
+              .orderBy('accountingDate', descending: true),
+          itemBuilder: (BuildContext context, DocumentSnapshot snapshot) {
+            final T.Transaction transaction = T.Transaction.parse(snapshot);
+            return TransactionListItemComponent(
+              transaction: transaction,
+              category: getCategory(transaction, categories),
+            );
+          },
+          loaderBuilder: (BuildContext context) => Text("Loading..."),
+          separatorBuilder: (BuildContext context) => Divider(
+            color: Colors.grey,
+          ),
         );
       },
     );
+  }
+
+  Category getCategory(T.Transaction transaction, List<Category> categories) {
+    for (Category category in categories) {
+      if (transaction.categoryId == category.id) {
+        return category;
+      }
+    }
+    return null;
   }
 }
