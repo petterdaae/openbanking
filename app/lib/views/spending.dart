390db202b6d141031a3d14cff516863a36fa54c6
Petter Daae <petter.daae@gmail.com>
2020-05-02 11:38:44 +0200

    Convert spending to stfw and fetch both transactions and categories

--- app/lib/views/spending.dart
+++ app/lib/views/spending.dart
@@ -2,31 +2,59 @@ import 'package:app/auth.dart';
 import 'package:app/components/spending.dart';
 import 'package:app/dependencies.dart';
 import 'package:app/models/category.dart';
+import 'package:app/models/transaction.dart' as T;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Spending extends StatelessWidget {
+import '../utilities.dart';
+
+class Spending extends StatefulWidget {
+  @override
+  _SpendingState createState() => _SpendingState();
+}
+
+class _SpendingState extends State<Spending> {
+  _SpendingState() {
+    final DateTime now = DateTime.now();
+    _dateTime = DateTime(now.year, now.month, 1);
+  }
+
+  DateTime _dateTime;
+
   @override
   Widget build(BuildContext context) {
     Firestore firestore = Provider.of<Dependencies>(context).firestore;
     String uid = Provider.of<Auth>(context).uid;
-    return StreamBuilder<QuerySnapshot>(
-      stream: firestore
+    return StreamBuilder2<QuerySnapshot, QuerySnapshot>(
+      stream1: firestore
           .collection('users')
           .document(uid)
           .collection('categories')
           .snapshots(),
+      stream2: firestore
+          .collection('users')
+          .document(uid)
+          .collection('transactions')
+          .where('accountingDate', isGreaterThanOrEqualTo: this._dateTime)
+          .snapshots(),
       builder: (
         BuildContext context,
-        AsyncSnapshot<QuerySnapshot> snapshot,
+        AsyncSnapshot<QuerySnapshot> snapshot1,
+        AsyncSnapshot<QuerySnapshot> snapshot2,
       ) {
-        if (!snapshot.hasData) return Text("loading");
+        if (!snapshot1.hasData || !snapshot2.hasData) return Text("loading");
+
         final List<Category> categories =
-            snapshot.data.documents.map((c) => Category.parse(c)).toList();
+            snapshot1.data.documents.map((c) => Category.parse(c)).toList();
+
+        final List<T.Transaction> transactions = snapshot2.data.documents
+            .map((c) => T.Transaction.parse(c))
+            .toList();
+
         return SpendingComponent(
-          dateTime: DateTime.now(),
-          transactions: [],
+          dateTime: this._dateTime,
+          transactions: transactions,
           onNextPressed: () {},
           onPrevPressed: () {},
         );
