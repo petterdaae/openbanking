a6452b2cc0f8adc729ff54287b49b53c300e9290
Petter Daae <petter.daae@gmail.com>
2020-05-03 16:12:52 +0200

    Start on category view

--- app/lib/views/categories.dart
+++ app/lib/views/categories.dart
@@ -1,8 +1,19 @@
+import 'package:app/views/category_list.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Categories"));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: CategoryList()),
+        ],
+      ),
+    );
   }
 }
