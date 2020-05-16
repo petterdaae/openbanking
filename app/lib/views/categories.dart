bae315d3004b25e114c872c14a10320d7e9e9458
Petter Daae <petter.daae@gmail.com>
2020-05-16 14:27:20 +0200

    Make it possible to delete categories

--- app/lib/views/categories.dart
+++ app/lib/views/categories.dart
@@ -1,11 +1,18 @@
 import 'package:app/views/category_list.dart';
 import 'package:app/views/new_category.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
+import '../providers/dependencies.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Auth auth = Provider.of<Auth>(context);
+    final Firestore firestore = Provider.of<Dependencies>(context).firestore;
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.of(context).push(
@@ -17,7 +24,34 @@ class Categories extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(child: CategoryList()),
+          Expanded(child: CategoryList(
+            onCategoryTapped: (id) {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      child: Wrap(
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () {
+                              firestore
+                                  .collection('users')
+                                  .document(auth.uid)
+                                  .collection('categories')
+                                  .document(id)
+                                  .delete();
+                              Navigator.of(context).pop();
+                            },
+                            child: ListTile(
+                              title: Text("Delete category"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
+          )),
         ],
       ),
     );
