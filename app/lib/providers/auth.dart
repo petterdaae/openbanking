cd1694321fa735df19af281aaaa360ce9c2e4def
Petter Daae <petter.daae@gmail.com>
2020-05-21 17:15:20 +0200

    Implement proper authentication in app

--- app/lib/providers/auth.dart
+++ app/lib/providers/auth.dart
@@ -4,31 +4,71 @@ import 'package:provider/provider.dart';
 
 import 'dependencies.dart';
 
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+}
+
 class Auth extends ChangeNotifier {
   Auth() {
-    _authenticated = false;
+    _authStatus = AuthStatus.NotAuthenticated;
     _uid = null;
   }
 
-  bool _authenticated;
+  AuthStatus _authStatus;
   String _uid;
 
-  bool get authenticated => _authenticated;
+  AuthStatus get authStatus => _authStatus;
   String get uid => _uid;
 
-  Future<bool> login(BuildContext context) async {
+  void listen(BuildContext context) {
+    var deps = Provider.of<Dependencies>(context, listen: false);
+    deps.auth.onAuthStateChanged.listen((user) {
+      if (user != null &&
+          _authStatus != AuthStatus.Authenticated &&
+          _authStatus != AuthStatus.Authenticating) {
+        _uid = user.uid;
+        _authStatus = AuthStatus.Authenticated;
+        notifyListeners();
+      }
+    });
+  }
+
+  Future<bool> login(
+    BuildContext context,
+    String email,
+    String password,
+  ) async {
+    _authStatus = AuthStatus.Authenticating;
+    notifyListeners();
     try {
       var deps = Provider.of<Dependencies>(context, listen: false);
       AuthResult authResult = await deps.auth.signInWithEmailAndPassword(
-        email: deps.email,
-        password: deps.password,
+        email: email,
+        password: password,
       );
-      _authenticated = true;
+      _authStatus = AuthStatus.Authenticated;
       _uid = authResult.user.uid;
+    } on Exception catch (ex) {
+      print(ex);
+      _authStatus = AuthStatus.NotAuthenticated;
+      notifyListeners();
+      return false;
+    }
+    notifyListeners();
+    return true;
+  }
+
+  Future<bool> signOut(BuildContext context) async {
+    var deps = Provider.of<Dependencies>(context, listen: false);
+    try {
+      await deps.auth.signOut();
     } on Exception catch (ex) {
       print(ex);
       return false;
     }
+    _authStatus = AuthStatus.NotAuthenticated;
     notifyListeners();
     return true;
   }
