c26e551fcd01eff2c5c313e6da7453a9b5f99509
Petter Daae <petter.daae@gmail.com>
2020-05-21 17:15:20 +0200

    Implement proper authentication in app

--- app/lib/views/settings.dart
+++ app/lib/views/settings.dart
@@ -1,8 +1,19 @@
+import 'package:app/providers/auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Settings"));
+    final Auth auth = Provider.of<Auth>(context);
+    return Center(
+      child: OutlineButton(
+        onPressed: () {
+          auth.signOut(context);
+        },
+        child: Text("Sign out"),
+      ),
+    );
   }
 }
