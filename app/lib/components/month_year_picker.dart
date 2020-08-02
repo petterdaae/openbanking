effce314197cc1feb9def1e7c2b44d9b7b209fe1
Petter Daae <petter.daae@gmail.com>
2020-08-02 13:12:20 +0200

    Change date/year picker in spendings

--- app/lib/components/month_year_picker.dart
+++ app/lib/components/month_year_picker.dart
@@ -1,5 +1,3 @@
-import 'package:app/components/round_icon_button.dart';
-import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -16,21 +14,69 @@ class MonthYearPicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final DateTime previousMonth =
+        DateTime(dateTime.year, dateTime.month - 1, dateTime.day);
+    final DateTime nextMonth =
+        DateTime(dateTime.year, dateTime.month + 1, dateTime.day);
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
-        RoundIconButton(
+        _MonthYearButton(
+          dateTime: previousMonth,
+          backgroundColor: Colors.grey,
           onPressed: this.onPrevPressed,
-          icon: CommunityMaterialIcons.arrow_left,
         ),
-        Text(DateFormat("MMMM").format(dateTime)),
-        Text("/"),
-        Text(DateFormat("y").format(dateTime)),
-        RoundIconButton(
+        _MonthYearButton(
+          dateTime: dateTime,
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        _MonthYearButton(
+          dateTime: nextMonth,
+          backgroundColor: Colors.grey,
           onPressed: this.onNextPressed,
-          icon: CommunityMaterialIcons.arrow_right,
         ),
       ],
+    );
+  }
+}
+
+class _MonthYearButton extends StatelessWidget {
+  const _MonthYearButton({
+    @required this.dateTime,
+    @required this.backgroundColor,
+    this.onPressed,
+  });
+
+  final DateTime dateTime;
+  final Color backgroundColor;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: InkWell(
+          onTap: this.onPressed,
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(50.0),
+              ),
+            ),
+            child: Text(
+              DateFormat("MMMM").format(dateTime) +
+                  " " +
+                  DateFormat("y").format(dateTime),
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
